12cff6eb65e9d48d2eca857bd51cd75aa59aa14a
Lorand Kallai <lorandkallai@gmail.com>
2021-08-28 11:35:09 +0200

    inital commit

---
+++ hello.ts
@@ -0,0 +1,15 @@
+console.log('Hello World!')
+
+function greet(person: string, date: Date) {
+  console.log(`Hello ${person}, today is ${date.toLocaleTimeString()}!`)
+}
+
+let bar
+function foo() { return true; }
+if (foo()) { bar = 0; }
+
+greet('Lorand', new Date())
+let a = 0
+a = 2 ? a = 25 :
+  a = 20 ? a = 15 : a = 20
+const c = 12
