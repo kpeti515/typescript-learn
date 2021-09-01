2de6cd1e9944f1f9ba64c042daa8f71fb5ceadb7
Lorand Kallai <lorandkallai@gmail.com>
2021-09-01 08:48:53 +0200

    stuffs from the another machine

--- hello.js
+++ hello.js
@@ -14,3 +14,48 @@ console.log(new Date());
 greet('Lorand', new Date());
 const c = 11;
 console.log(c * 2);
+// equality narrowing
+function example(x, y) {
+  if (x === y) {
+    x.toLowerCase();
+    y.toUpperCase();
+  } else {
+    console.log(x, y);
+  }
+}
+example('asd', 'lol');
+function multiplyValue(container, factor) {
+  if (container.value != null) {
+    console.log(container.value);
+    const multipliedValue = container.value * factor;
+    console.log(multipliedValue);
+  }
+}
+multiplyValue({ value: 22 }, 3);
+function move(animal) {
+  if ('swim' in animal) {
+    return animal.swim();
+  }
+  return animal.fly();
+}
+const bird = {
+  swim: () => console.log('I can swim')
+};
+const fish = {
+  fly: () => console.log('I can fly')
+};
+const human = {
+  swim: () => console.log('I can swim')
+};
+move(bird);
+move(fish);
+move(human);
+function logTime(x) {
+  if (x instanceof Date) {
+    console.log(x.toUTCString());
+  } else {
+    console.log(x.toLowerCase());
+  }
+}
+logTime(new Date());
+logTime('today');
--- hello.ts
+++ hello.ts
@@ -19,3 +19,60 @@ greet('Lorand', new Date());
 const c = 11;
 
 console.log(c * 2);
+// equality narrowing
+function example(x: string | number, y: string | boolean) {
+  if (x === y) {
+    x.toLowerCase();
+    y.toUpperCase();
+  } else {
+    console.log(x, y);
+  }
+}
+example('asd', 'lol');
+
+interface Container {
+  value: number | null | undefined;
+}
+function multiplyValue(container: Container, factor: number) {
+  if (container.value != null) {
+    console.log(container.value);
+    const multipliedValue = container.value * factor;
+    console.log(multipliedValue);
+  }
+}
+multiplyValue({ value: 22 }, 3);
+
+// the in operator narrowing
+type Fish = { swim: () => void };
+type Bird = { fly: () => void };
+type Human = { swim?: () => void; fly?: () => void };
+
+function move(animal: Fish | Bird | Human) {
+  if ('swim' in animal) {
+    return animal.swim();
+  }
+  return animal.fly();
+}
+const bird = {
+  swim: () => console.log('I can swim')
+};
+const fish = {
+  fly: () => console.log('I can fly')
+};
+const human = {
+  swim: () => console.log('I can swim')
+};
+move(bird);
+move(fish);
+move(human);
+
+// instanceof narrowing
+function logTime(x: Date | string) {
+  if (x instanceof Date) {
+    console.log(x.toUTCString());
+  } else {
+    console.log(x.toLowerCase());
+  }
+}
+logTime(new Date());
+logTime('today');
