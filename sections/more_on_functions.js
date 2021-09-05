05a03d80dccaa8c01c8e9f01c472211004eb6938
Kallai Lorand <lorandkallai@gmail.com>
2021-09-05 19:59:10 +0200

    section->more on functions

--- sections/more_on_functions.js
+++ sections/more_on_functions.js
@@ -29,3 +29,43 @@ function firstElement(arr) {
 const firstString = firstElement(['asd', 'lol', 'meh']);
 const firstNumber = firstElement(Array.from({ length: 5 }, () => Math.ceil(Math.random() * 10)));
 console.log(firstString, firstNumber);
+// Inference
+function map(arr, func) {
+  return arr.map(func);
+}
+const parsed = map(['1', '2', '3'], (number) => parseInt(number, 10));
+console.log(parsed);
+// Constrains
+function longest(a, b) {
+  if (a.length >= b.length) {
+    return a;
+  }
+  return b;
+}
+const longerArray = longest([1, 2], [1, 2, 3, 4, 5]);
+const longerString = longest('Lorand', 'Andi');
+console.log(longerArray, longerString);
+// Working with Constrained Values
+// -> be careful what to return
+// function minimumLength<Type extends { length: number }>(
+//   obj: Type,
+//   minimum: number
+// ): Type {
+//   if (obj.length >= minimum) {
+//     return obj;
+//   }
+//   return { length: minimum }; --->Type '{ length: number; }' is not assignable to type 'Type'.
+//   '{ length: number; }' is assignable to the constraint of type 'Type', but 'Type' could be instantiated with a different subtype of constraint '{ length: number; }'.
+// }
+// 'arr' gets value { length: 6 }
+// const arr = minimumLength([1, 2, 3], 6);
+// and crashes here because arrays have
+// a 'slice' method, but not the returned object!
+// console.log(arr.slice(0));
+// Specifying Type Arguments
+function combine(arr1, arr2) {
+  return arr1.concat(arr2);
+}
+const combinedArr = combine([1, 2, 3], ['hello']);
+console.log(combinedArr);
+// guidelines for writing good generic functions
--- sections/more_on_functions.ts
+++ sections/more_on_functions.ts
@@ -7,7 +7,6 @@ function printToConsole(s: string) {
   console.log(s);
 }
 greeter(printToConsole);
-
 // call signatures
 type describableFunction = {
   description: string;
@@ -49,3 +48,49 @@ function firstElement<Type>(arr: Type[]): Type {
 const firstString = firstElement(['asd', 'lol', 'meh']);
 const firstNumber = firstElement(Array.from({ length: 5 }, () => Math.ceil(Math.random() * 10)));
 console.log(firstString, firstNumber);
+
+// Inference
+function map<Input, Output>(arr: Input[], func: (arg: Input) => Output): Output[] {
+  return arr.map(func);
+}
+const parsed = map(['1', '2', '3'], (number) => parseInt(number, 10));
+console.log(parsed);
+
+// Constrains
+function longest<Type extends { length: number }>(a: Type, b: Type) {
+  if (a.length >= b.length) {
+    return a;
+  }
+  return b;
+}
+const longerArray = longest([1, 2], [1, 2, 3, 4, 5]);
+const longerString = longest('Lorand', 'Andi');
+console.log(longerArray, longerString);
+
+// Working with Constrained Values
+// -> be careful what to return
+// function minimumLength<Type extends { length: number }>(
+//   obj: Type,
+//   minimum: number
+// ): Type {
+//   if (obj.length >= minimum) {
+//     return obj;
+//   }
+//   return { length: minimum }; --->Type '{ length: number; }' is not assignable to type 'Type'.
+//   '{ length: number; }' is assignable to the constraint of type 'Type', but 'Type' could be instantiated with a different subtype of constraint '{ length: number; }'.
+// }
+
+// 'arr' gets value { length: 6 }
+// const arr = minimumLength([1, 2, 3], 6);
+// and crashes here because arrays have
+// a 'slice' method, but not the returned object!
+// console.log(arr.slice(0));
+
+// Specifying Type Arguments
+function combine<Type>(arr1: Type[], arr2: Type[]): Type[] {
+  return arr1.concat(arr2);
+}
+const combinedArr = combine<string | number>([1, 2, 3], ['hello']);
+console.log(combinedArr);
+
+// guidelines for writing good generic functions
